305a07822e0865978ed90e5f0e364b1fe86808f9
Benjamin Vankat <bvankat@gmail.com>
2025-11-07 14:03:18 -0600

    Plugin name, author

--- modal-builder.php
+++ modal-builder.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:       Modal Builder
+ * Plugin Name:       Easy Pop - Perfect Popups for Wordpress
  * Description:       Create beautiful modals and popups using the WordPress block editor with powerful targeting and display options.
  * Version:           0.1.0
  * Requires at least: 6.1
  * Requires PHP:      7.4
- * Author:            WordPress Telex
+ * Author:            Hanscom Park Studio
  * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       modal-builder
